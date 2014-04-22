1bfb640b55d8195efbeeaaf09f23a8e28ac6643c
Connor Regan <connor@1871.com>
2014-04-22 23:39:56 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coolapp::Application.config.secret_key_base = '3a1cf55f32a31ac29bbce91800c2258088d92358bee42e49b0708812f86ed415c4e2d2a5827140cb1c0a8dc2a3d4e7d87dc934bc74797ea482c8a516127b9c6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Coolapp::Application.config.session_store :cookie_store, key: '_coolapp_session'
